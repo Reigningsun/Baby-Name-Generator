918ca60f1c35ff46414e0207ec776d39f8562ff9
Reigningsun <38302744+Reigningsun@users.noreply.github.com>
2018-06-08 15:59:29 -0500

    Update LetterProb.java
    Reformatted for Github

--- LetterProb.java
+++ LetterProb.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class LetterProb {
-	String characters;														// Stores the characters we are looking at
-	ArrayList<String> nextLetter;											// Stores each instance of a next letter seen in the training set to be used in randomly selecting a letter later
+	String characters;								// Stores the characters we are looking at
+	ArrayList<String> nextLetter;							// Stores each instance of a next letter seen in the training set to be used in randomly selecting a letter later
 
 	LetterProb (String characters){
 		this.characters = characters;
